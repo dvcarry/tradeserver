a1aa7615c03e3468ce88a8ecaf48fd9058a83bb6
Kirill <dvcarry@gmail.com>
2021-04-11 21:55:24 +0300

    add orders_logs

--- routes/orders.js
+++ routes/orders.js
@@ -7,12 +7,15 @@ router.get("/", async (req, res) => {
 
     try {
         const { rows: orders } = await pool.query('SELECT orders.*, SUM(basket.price * basket.amount) AS summ FROM orders LEFT JOIN basket ON orders.id = basket.order_id GROUP BY orders.id ORDER BY orders.id DESC', [])
-        const { rows: products } = await pool.query('SELECT basket.order_id, basket.price, basket.amount, products.name FROM basket LEFT JOIN products ON basket.product_id = products.id', [])
+        const { rows: products } = await pool.query('SELECT basket.order_id, basket.price, basket.amount, products.name, products.model FROM basket LEFT JOIN products ON basket.product_id = products.id', [])
+        const { rows: orders_logs } = await pool.query('SELECT * FROM orders_logs', [])
         const ordersWithProducts = orders.map(item => {
             const ordersProducts = products.filter(product => product.order_id === item.id)
+            const order_logs = orders_logs.filter(log => log.order_id === tem.id)
             return {
                 ...item,
-                ordersProducts
+                ordersProducts,
+                order_logs
             }
         })
         res.send(ordersWithProducts)
@@ -66,10 +69,26 @@ router.post("/", async (req, res) => {
         const userRes = await pool.query('SELECT * FROM users WHERE user_id = $1', [user_id])
         const user = userRes.rows[0]
         const number = user.id.toString() + Math.floor(Math.random() * (999999 - 100000 + 1) + 100000)
-        const orderRes = await pool.query('INSERT INTO orders (contacts, address, date, status, user_id, number, type) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *', [user.phone, user.address, today, 'обработка', user_id, +number, user.type])
+        const orderRes = await pool.query('INSERT INTO orders (contacts, address, date, status, user_id, number, type, email) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *', [user.phone, user.address, today, 'обработка', user_id, +number, user.type, user.email])
         const order_id = orderRes.rows[0].id
         await pool.query('UPDATE basket SET order_id = $1 WHERE user_id = $2 AND order_id = 0', [order_id, user_id])
+        await pool.query('INSERT INTO orders_logs (order_id, status, text, date) VALUES ($1, $2, $3, $4) RETURNING *', [order_id, 'обработка', 'Новый заказ', today])
+
         res.json(number)
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+router.post("/logs", async (req, res) => {
+
+    const { order_id, status, text } = req.body
+
+    const date = new Date()
+
+    try {
+        const orderRes = await pool.query('INSERT INTO orders_logs (order_id, status, text, date) VALUES ($1, $2, $3, $4) RETURNING *', [order_id, status, text, date])
+        res.json(orderRes.rows[0])
     } catch (error) {
         console.log(error)
     }
